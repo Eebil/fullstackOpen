095401e74e6d741e5b8e6cfc5adcf86f8648817c
Eetu Rantala <eetu.rantala@hotmail.com>
2023-02-13 12:12:28 +0200

    part 2 Done

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,16 +1,19 @@
 import { calculateNewValue } from '@testing-library/user-event/dist/utils'
 import { useState, useEffect } from 'react'
 import personService from './services/persons'
-import axios from 'axios'
 import Filter from './components/Filter.js'
 import PersonForm from './components/PersonForm.js'
 import PersonTable from './components/PersonTable.js'
+import Error from './components/Error'
+import Notification from './components/Notification'
 
 const App = () => {
   const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [showFilter, setShowFilter] = useState(new RegExp('', 'i'))
+  const [notificationMessage, setNotificationMessage] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   // initial load from Json-server
   useEffect(() => {
@@ -20,6 +23,20 @@ const App = () => {
         setPersons(response.data)
       })
   }, [])
+
+  const flashNotification = (message) => {
+    setNotificationMessage(message)
+    setTimeout(() => {
+      setNotificationMessage(null)
+    }, 5000)
+  }
+
+  const flashError = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 5000)
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -32,31 +49,33 @@ const App = () => {
 
     if (persons.find(person => person.name === newName)) {
       if (window.confirm(`${newName} is already added to phonebook, do you wish to update their information?`)){
-        personService
-        .update(persons.find(person => person.name === newName).id, newPerson)
-        .then(response => {
-          setPersons(persons.map(person => person.id !== persons.find(person => person.name === newName).id ? person : response.data))
-          setNewName('')
-          setNewNumber('') // DRY refactor later
-        })
-        return (
-          alert(`${newName} updated!`)
-        )
+          personService
+          .update(persons.find(person => person.name === newName).id, newPerson)
+          .then(response => {
+            setPersons(persons.map(person => person.id !== persons.find(person => person.name === newName).id ? person : response.data))
+            flashNotification(`Note: '${newName}' information updated!`)
+            setNewName('')
+            setNewNumber('')
+          })
+          .catch(error => {
+            flashError(error.message)
+          })
       }
       else {
-        return (
-          alert(`${newName} remains unchanged`)
-        )
+        flashNotification(`No changes made`)
       }
     }
 
-    personService
-    .create(newPerson)
-    .then(response => {
-      setPersons(persons.concat(response.data))
-      setNewName('')
-      setNewNumber('')
-    })
+    else {
+      personService
+      .create(newPerson)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+        flashNotification(`Note: ${newName} added to the phonebook`)
+        setNewName('')
+        setNewNumber('')
+      })
+    }
   }
 
   const handleDelete = (id) => {
@@ -64,7 +83,7 @@ const App = () => {
         personService
         .destroy(id)
         .then(response => {
-          console.log(response.data)
+          flashNotification(`Note: ${persons.find(person => person.id === id).name} Deleted`)
           setPersons(persons.filter(person => person.id !== id))
         })
       }
@@ -76,9 +95,13 @@ const App = () => {
   const handlePhoneInput = (event) => setNewNumber(event.target.value)
 
 
+
+
   return (
     <div>
       <h1>Phonebook</h1>
+      < Notification message={notificationMessage} />
+      < Error message={errorMessage} />
       < Filter      handleFilter={handleFilter} />
       < PersonForm  handleSubmit={handleSubmit}
                     handleNameInput={handleNameInput}
